f1a429b3bb089455ce3b784544d610e3b20b89b4
Aditya <officialdesaiaditya@gmail.com>
2024-08-23 19:28:33 +0530

    Add call methods

--- src/app.js
+++ src/app.js
@@ -7,7 +7,8 @@ import { verifyToken } from './middlewares/verifyToken.middleware.js';
 const app = express();
 
 app.use(cors({
-    origin: process.env.CLIENT_URL
+    origin: process.env.CLIENT_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
 app.use(express.json());
 app.use(express.urlencoded());
