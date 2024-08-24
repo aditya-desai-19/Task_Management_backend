13d992bd1f054efd10ba620bb01217d92d3394b1
Aditya <officialdesaiaditya@gmail.com>
2024-08-24 22:01:03 +0530

    Update cors allowed all origin

--- src/app.js
+++ src/app.js
@@ -6,10 +6,7 @@ import { verifyToken } from './middlewares/verifyToken.middleware.js';
 
 const app = express();
 
-app.use(cors({
-    origin: process.env.CLIENT_URL,
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded());
